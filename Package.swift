b76adf1aba554a4b5b84a546afef4a964bdca3c4
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-16 18:34:33 +1200

    Rename to Plinth

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "QRIMatrix",
+    name: "Plinth",
     platforms: [
         .macOS(.v12),
         .iOS(.v13)
     ],
     products: [
-        .library(name: "QRIMatrix", targets: ["QRIMatrix"])
+        .library(name: "Plinth", targets: ["Plinth"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0")
     ],
     targets: [
         .target(
-            name: "QRIMatrix",
+            name: "Plinth",
             dependencies: [
                 .product(name: "Numerics", package: "swift-numerics")
             ]
