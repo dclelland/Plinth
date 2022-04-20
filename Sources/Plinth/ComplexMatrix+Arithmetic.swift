64eff497c311645205702464cefdb86332c55372
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-20 17:25:25 +1200

    Implement ComplexMatrix arithmetic

--- Sources/Plinth/ComplexMatrix+Arithmetic.swift
+++ Sources/Plinth/ComplexMatrix+Arithmetic.swift
@@ -6,12 +6,379 @@
 //
 
 import Foundation
-import Accelerate
 
 extension ComplexMatrix where Scalar == Float {
+    
+    public static prefix func + (right: ComplexMatrix) -> ComplexMatrix {
+        return right
+    }
+    
+    public static prefix func - (right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: -right.real, imaginary: -right.imaginary)
+    }
+    
+    public static func + (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right, imaginary: left.imaginary + right)
+    }
+
+    public static func + (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left + right.real, imaginary: left + right.imaginary)
+    }
+    
+    public static func + (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right.real, imaginary: left.imaginary + right.imaginary)
+    }
+    
+    public static func + (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right.real, imaginary: left.imaginary + right.imaginary)
+    }
+    
+    public static func + (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right, imaginary: left.imaginary + right)
+    }
+
+    public static func + (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left + right.real, imaginary: left + right.imaginary)
+    }
+
+    public static func + (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right.real, imaginary: left.imaginary + right.imaginary)
+    }
+
+    public static func - (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right, imaginary: left.imaginary - right)
+    }
+
+    public static func - (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left - right.real, imaginary: left - right.imaginary)
+    }
+    
+    public static func - (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right.real, imaginary: left.imaginary - right.imaginary)
+    }
+    
+    public static func - (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right.real, imaginary: left.imaginary - right.imaginary)
+    }
+    
+    public static func - (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right, imaginary: left.imaginary - right)
+    }
+
+    public static func - (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left - right.real, imaginary: left - right.imaginary)
+    }
+
+    public static func - (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right.real, imaginary: left.imaginary - right.imaginary)
+    }
+
+    public static func * (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right, imaginary: left.imaginary * right)
+    }
+
+    public static func * (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left * right.real, imaginary: left * right.imaginary)
+    }
+    
+    public static func * (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right.real, imaginary: left.imaginary * right.imaginary)
+    }
+    
+    public static func * (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right.real, imaginary: left.imaginary * right.imaginary)
+    }
+    
+    public static func * (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right, imaginary: left.imaginary * right)
+    }
+
+    public static func * (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left * right.real, imaginary: left * right.imaginary)
+    }
+
+    public static func * (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right.real, imaginary: left.imaginary * right.imaginary)
+    }
+
+    public static func / (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right, imaginary: left.imaginary / right)
+    }
+
+    public static func / (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left / right.real, imaginary: left / right.imaginary)
+    }
+    
+    public static func / (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right.real, imaginary: left.imaginary / right.imaginary)
+    }
+    
+    public static func / (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right.real, imaginary: left.imaginary / right.imaginary)
+    }
+    
+    public static func / (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right, imaginary: left.imaginary / right)
+    }
+
+    public static func / (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left / right.real, imaginary: left / right.imaginary)
+    }
+
+    public static func / (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right.real, imaginary: left.imaginary / right.imaginary)
+    }
+    
+    public static func += (left: inout ComplexMatrix, right: Scalar) {
+        left = left + right
+    }
+    
+    public static func += (left: inout ComplexMatrix, right: Complex) {
+        left = left + right
+    }
+    
+    public static func += (left: inout ComplexMatrix, right: Matrix) {
+        left = left + right
+    }
+
+    public static func += (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left + right
+    }
+    
+    public static func -= (left: inout ComplexMatrix, right: Scalar) {
+        left = left - right
+    }
+    
+    public static func -= (left: inout ComplexMatrix, right: Complex) {
+        left = left - right
+    }
+    
+    public static func -= (left: inout ComplexMatrix, right: Matrix) {
+        left = left - right
+    }
+
+    public static func -= (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left - right
+    }
+
+    public static func *= (left: inout ComplexMatrix, right: Scalar) {
+        left = left * right
+    }
+    
+    public static func *= (left: inout ComplexMatrix, right: Complex) {
+        left = left * right
+    }
+    
+    public static func *= (left: inout ComplexMatrix, right: Matrix) {
+        left = left * right
+    }
+
+    public static func *= (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left * right
+    }
+
+    public static func /= (left: inout ComplexMatrix, right: Scalar) {
+        left = left / right
+    }
+    
+    public static func /= (left: inout ComplexMatrix, right: Complex) {
+        left = left / right
+    }
+    
+    public static func /= (left: inout ComplexMatrix, right: Matrix) {
+        left = left / right
+    }
+
+    public static func /= (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left / right
+    }
     
 }
 
 extension ComplexMatrix where Scalar == Double {
+    
+    public static prefix func + (right: ComplexMatrix) -> ComplexMatrix {
+        return right
+    }
+    
+    public static prefix func - (right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: -right.real, imaginary: -right.imaginary)
+    }
+    
+    public static func + (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right, imaginary: left.imaginary + right)
+    }
+
+    public static func + (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left + right.real, imaginary: left + right.imaginary)
+    }
+    
+    public static func + (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right.real, imaginary: left.imaginary + right.imaginary)
+    }
+    
+    public static func + (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right.real, imaginary: left.imaginary + right.imaginary)
+    }
+    
+    public static func + (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right, imaginary: left.imaginary + right)
+    }
+
+    public static func + (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left + right.real, imaginary: left + right.imaginary)
+    }
+
+    public static func + (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real + right.real, imaginary: left.imaginary + right.imaginary)
+    }
+
+    public static func - (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right, imaginary: left.imaginary - right)
+    }
+
+    public static func - (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left - right.real, imaginary: left - right.imaginary)
+    }
+    
+    public static func - (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right.real, imaginary: left.imaginary - right.imaginary)
+    }
+    
+    public static func - (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right.real, imaginary: left.imaginary - right.imaginary)
+    }
+    
+    public static func - (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right, imaginary: left.imaginary - right)
+    }
+
+    public static func - (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left - right.real, imaginary: left - right.imaginary)
+    }
+
+    public static func - (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real - right.real, imaginary: left.imaginary - right.imaginary)
+    }
+
+    public static func * (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right, imaginary: left.imaginary * right)
+    }
+
+    public static func * (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left * right.real, imaginary: left * right.imaginary)
+    }
+    
+    public static func * (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right.real, imaginary: left.imaginary * right.imaginary)
+    }
+    
+    public static func * (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right.real, imaginary: left.imaginary * right.imaginary)
+    }
+    
+    public static func * (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right, imaginary: left.imaginary * right)
+    }
+
+    public static func * (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left * right.real, imaginary: left * right.imaginary)
+    }
+
+    public static func * (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real * right.real, imaginary: left.imaginary * right.imaginary)
+    }
+
+    public static func / (left: ComplexMatrix, right: Scalar) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right, imaginary: left.imaginary / right)
+    }
+
+    public static func / (left: Scalar, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left / right.real, imaginary: left / right.imaginary)
+    }
+    
+    public static func / (left: ComplexMatrix, right: Complex) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right.real, imaginary: left.imaginary / right.imaginary)
+    }
+    
+    public static func / (left: Complex, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right.real, imaginary: left.imaginary / right.imaginary)
+    }
+    
+    public static func / (left: ComplexMatrix, right: Matrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right, imaginary: left.imaginary / right)
+    }
+
+    public static func / (left: Matrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left / right.real, imaginary: left / right.imaginary)
+    }
+
+    public static func / (left: ComplexMatrix, right: ComplexMatrix) -> ComplexMatrix {
+        return ComplexMatrix(real: left.real / right.real, imaginary: left.imaginary / right.imaginary)
+    }
+    
+    public static func += (left: inout ComplexMatrix, right: Scalar) {
+        left = left + right
+    }
+    
+    public static func += (left: inout ComplexMatrix, right: Complex) {
+        left = left + right
+    }
+    
+    public static func += (left: inout ComplexMatrix, right: Matrix) {
+        left = left + right
+    }
+
+    public static func += (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left + right
+    }
+    
+    public static func -= (left: inout ComplexMatrix, right: Scalar) {
+        left = left - right
+    }
+    
+    public static func -= (left: inout ComplexMatrix, right: Complex) {
+        left = left - right
+    }
+    
+    public static func -= (left: inout ComplexMatrix, right: Matrix) {
+        left = left - right
+    }
+
+    public static func -= (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left - right
+    }
+
+    public static func *= (left: inout ComplexMatrix, right: Scalar) {
+        left = left * right
+    }
+    
+    public static func *= (left: inout ComplexMatrix, right: Complex) {
+        left = left * right
+    }
+    
+    public static func *= (left: inout ComplexMatrix, right: Matrix) {
+        left = left * right
+    }
+
+    public static func *= (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left * right
+    }
+
+    public static func /= (left: inout ComplexMatrix, right: Scalar) {
+        left = left / right
+    }
+    
+    public static func /= (left: inout ComplexMatrix, right: Complex) {
+        left = left / right
+    }
+    
+    public static func /= (left: inout ComplexMatrix, right: Matrix) {
+        left = left / right
+    }
+
+    public static func /= (left: inout ComplexMatrix, right: ComplexMatrix) {
+        left = left / right
+    }
     
 }
