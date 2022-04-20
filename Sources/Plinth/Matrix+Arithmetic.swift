6efd2fe2b450c2419a26e75bf56fbf5ef4d56538
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-20 16:05:29 +1200

    Stash Arithmetic sketches

--- Sources/Plinth/Matrix+Arithmetic.swift
+++ Sources/Plinth/Matrix+Arithmetic.swift
@@ -8,10 +8,58 @@
 import Foundation
 import Accelerate
 
-extension Matrix where Scalar == Float {
+public protocol MatrixArithmetic {
+    
+    associatedtype Scalar
+    
+    static prefix func + (right: Self) -> Self
+    static prefix func - (right: Self) -> Self
+    
+    static func + (left: Self, right: Scalar) -> Self
+    static func + (left: Scalar, right: Self) -> Self
+    static func + (left: Self, right: Self) -> Self
     
 }
 
-extension Matrix where Scalar == Double {
+extension Matrix: MatrixArithmetic where Scalar == Double {
+    
+    public static prefix func + (right: Matrix<Scalar>) -> Matrix<Scalar> {
+        return right
+    }
+    
+    public static prefix func - (right: Matrix<Scalar>) -> Matrix<Scalar> {
+        return Matrix<Double>(size: right.size, elements: vDSP.negative(right))
+    }
+    
+    public static func + (left: Matrix<Scalar>, right: Double) -> Matrix<Scalar> {
+        return Matrix<Double>(size: left.size, elements: vDSP.add(right, left))
+    }
+    
+    public static func + (left: Double, right: Matrix<Scalar>) -> Matrix<Scalar> {
+        return Matrix<Double>(size: right.size, elements: vDSP.add(left, right))
+    }
+    
+    public static func + (left: Matrix<Scalar>, right: Matrix<Scalar>) -> Matrix<Scalar> {
+        return Matrix<Double>(size: left.size, elements: vDSP.add(left, right))
+    }
+    
+    
+//    public static func + (left: Self, right: Scalar) -> Self {
+//        return Self(size: size, elements: vDSP.add(right, self))
+//    }
+//
+//    public static func + (left: Scalar, right: Self) -> Self {
+//        return Self(size: size, elements: vDSP.add(left, self))
+//    }
     
 }
+
+    
+
+//extension Matrix: MatrixArithmetic where Scalar == Float {
+//
+//}
+//
+//extension Matrix: MatrixArithmetic where Scalar == Double {
+//
+//}
