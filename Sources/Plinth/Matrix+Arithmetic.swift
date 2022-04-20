cf650d2fd9fcb7af1326e4d5288f6bb1fba2dabd
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-20 17:02:08 +1200

    Implement Matrix arithmetic without resorting to protocol soup

--- Sources/Plinth/Matrix+Arithmetic.swift
+++ Sources/Plinth/Matrix+Arithmetic.swift
@@ -8,68 +8,7 @@
 import Foundation
 import Accelerate
 
-public protocol MatrixArithmetic {
-    
-    associatedtype Scalar
-    
-    static prefix func + (matrix: Self) -> Self
-    static prefix func - (matrix: Self) -> Self
-    
-    static func + (left: Self, right: Scalar) -> Self
-    static func + (left: Scalar, right: Self) -> Self
-    static func + (left: Self, right: Self) -> Self
-    
-    static func - (left: Self, right: Scalar) -> Self
-    static func - (left: Scalar, right: Self) -> Self
-    static func - (left: Self, right: Self) -> Self
-    
-    static func * (left: Self, right: Scalar) -> Self
-    static func * (left: Scalar, right: Self) -> Self
-    static func * (left: Self, right: Self) -> Self
-    
-    static func / (left: Self, right: Scalar) -> Self
-    static func / (left: Scalar, right: Self) -> Self
-    static func / (left: Self, right: Self) -> Self
-    
-}
-
-extension MatrixArithmetic {
-    
-    public static func += (left: inout Self, right: Scalar) {
-        left = left + right
-    }
-    
-    public static func += (left: inout Self, right: Self) {
-        left = left + right
-    }
-    
-    public static func -= (left: inout Self, right: Scalar) {
-        left = left - right
-    }
-    
-    public static func -= (left: inout Self, right: Self) {
-        left = left - right
-    }
-    
-    public static func *= (left: inout Self, right: Scalar) {
-        left = left * right
-    }
-    
-    public static func *= (left: inout Self, right: Self) {
-        left = left * right
-    }
-    
-    public static func /= (left: inout Self, right: Scalar) {
-        left = left / right
-    }
-    
-    public static func /= (left: inout Self, right: Self) {
-        left = left / right
-    }
-    
-}
-
-extension Matrix: MatrixArithmetic where Scalar == Double {
+extension Matrix where Scalar == Float {
     
     public static prefix func + (matrix: Matrix) -> Matrix {
         return matrix
@@ -92,11 +31,11 @@ extension Matrix: MatrixArithmetic where Scalar == Double {
     }
     
     public static func - (left: Matrix, right: Scalar) -> Matrix {
-        return Matrix(size: left.size, elements: vDSP.add(-right, left))
+        return -right + left
     }
     
     public static func - (left: Scalar, right: Matrix) -> Matrix {
-        return Matrix(size: right.size, elements: vDSP.add(-left, right))
+        return -right + left
     }
     
     public static func - (left: Matrix, right: Matrix) -> Matrix {
@@ -125,6 +64,130 @@ extension Matrix: MatrixArithmetic where Scalar == Double {
     
     public static func / (left: Matrix, right: Matrix) -> Matrix {
         return Matrix(size: left.size, elements: vDSP.divide(left, right))
+    }
+    
+    public static func += (left: inout Matrix, right: Scalar) {
+        left = left + right
+    }
+    
+    public static func += (left: inout Matrix, right: Matrix) {
+        left = left + right
+    }
+    
+    public static func -= (left: inout Matrix, right: Scalar) {
+        left = left - right
+    }
+    
+    public static func -= (left: inout Matrix, right: Matrix) {
+        left = left - right
+    }
+    
+    public static func *= (left: inout Matrix, right: Scalar) {
+        left = left * right
+    }
+    
+    public static func *= (left: inout Matrix, right: Matrix) {
+        left = left * right
+    }
+    
+    public static func /= (left: inout Matrix, right: Scalar) {
+        left = left / right
+    }
+    
+    public static func /= (left: inout Matrix, right: Matrix) {
+        left = left / right
+    }
+    
+}
+
+extension Matrix where Scalar == Double {
+    
+    public static prefix func + (matrix: Matrix) -> Matrix {
+        return matrix
+    }
+    
+    public static prefix func - (matrix: Matrix) -> Matrix {
+        return Matrix(size: matrix.size, elements: vDSP.negative(matrix))
+    }
+    
+    public static func + (left: Matrix, right: Scalar) -> Matrix {
+        return Matrix(size: left.size, elements: vDSP.add(right, left))
+    }
+    
+    public static func + (left: Scalar, right: Matrix) -> Matrix {
+        return Matrix(size: right.size, elements: vDSP.add(left, right))
+    }
+    
+    public static func + (left: Matrix, right: Matrix) -> Matrix {
+        return Matrix(size: left.size, elements: vDSP.add(left, right))
+    }
+    
+    public static func - (left: Matrix, right: Scalar) -> Matrix {
+        return -right + left
+    }
+    
+    public static func - (left: Scalar, right: Matrix) -> Matrix {
+        return -right + left
+    }
+    
+    public static func - (left: Matrix, right: Matrix) -> Matrix {
+        return Matrix(size: left.size, elements: vDSP.subtract(left, right))
+    }
+    
+    public static func * (left: Matrix, right: Scalar) -> Matrix {
+        return Matrix(size: left.size, elements: vDSP.multiply(right, left))
+    }
+    
+    public static func * (left: Scalar, right: Matrix) -> Matrix {
+        return Matrix(size: right.size, elements: vDSP.multiply(left, right))
+    }
+    
+    public static func * (left: Matrix, right: Matrix) -> Matrix {
+        return Matrix(size: left.size, elements: vDSP.multiply(left, right))
+    }
+    
+    public static func / (left: Matrix, right: Scalar) -> Matrix {
+        return Matrix(size: left.size, elements: vDSP.divide(left, right))
+    }
+    
+    public static func / (left: Scalar, right: Matrix) -> Matrix {
+        return Matrix(size: right.size, elements: vDSP.divide(left, right))
+    }
+    
+    public static func / (left: Matrix, right: Matrix) -> Matrix {
+        return Matrix(size: left.size, elements: vDSP.divide(left, right))
+    }
+    
+    public static func += (left: inout Matrix, right: Scalar) {
+        left = left + right
+    }
+    
+    public static func += (left: inout Matrix, right: Matrix) {
+        left = left + right
+    }
+    
+    public static func -= (left: inout Matrix, right: Scalar) {
+        left = left - right
+    }
+    
+    public static func -= (left: inout Matrix, right: Matrix) {
+        left = left - right
+    }
+    
+    public static func *= (left: inout Matrix, right: Scalar) {
+        left = left * right
+    }
+    
+    public static func *= (left: inout Matrix, right: Matrix) {
+        left = left * right
+    }
+    
+    public static func /= (left: inout Matrix, right: Scalar) {
+        left = left / right
+    }
+    
+    public static func /= (left: inout Matrix, right: Matrix) {
+        left = left / right
     }
     
 }
