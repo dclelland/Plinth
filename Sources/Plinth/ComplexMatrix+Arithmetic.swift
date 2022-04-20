adaf43d093d72c57fc769b38cf944449f6b74f82
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-20 12:11:50 +1200

    Add arithmetic files

---
+++ Sources/Plinth/ComplexMatrix+Arithmetic.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexMatrix+Arithmetic.swift
+//  Plinth
+//
+//  Created by Daniel Clelland on 20/04/22.
+//
+
+import Foundation
+import Accelerate
+
+extension ComplexMatrix where Scalar == Float {
+    
+}
+
+extension ComplexMatrix where Scalar == Double {
+    
+}
---
+++ Sources/Plinth/Matrix+Arithmetic.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix+Arithmetic.swift
+//  Plinth
+//
+//  Created by Daniel Clelland on 20/04/22.
+//
+
+import Foundation
+import Accelerate
+
+extension Matrix where Scalar == Float {
+    
+}
+
+extension Matrix where Scalar == Double {
+    
+}
