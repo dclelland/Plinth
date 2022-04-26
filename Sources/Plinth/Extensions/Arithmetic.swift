5cb9226ad2b77d03476171f16d9eb37084b98932
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-26 12:02:30 +1200

    Unify arithmetic module

--- Sources/Plinth/Extensions/Arithmetic.swift
+++ Sources/Plinth/Extensions/Arithmetic.swift
@@ -1,5 +1,5 @@
 //
-//  Matrix+Arithmetic.swift
+//  Array+Arithmetic.swift
 //  Plinth
 //
 //  Created by Daniel Clelland on 20/04/22.
@@ -7,6 +7,118 @@
 
 import Foundation
 import Accelerate
+
+extension Array where Element == Float {
+    
+    @inlinable public static prefix func + (operand: Array) -> Array {
+        return operand
+    }
+    
+    @inlinable public static prefix func - (operand: Array) -> Array {
+        return vDSP.negative(operand)
+    }
+    
+}
+
+extension Array where Element == Float {
+    
+    @inlinable public static func + (left: Array, right: Element) -> Array {
+        return vDSP.add(right, left)
+    }
+    
+    @inlinable public static func - (left: Array, right: Element) -> Array {
+        return -right + left
+    }
+    
+    @inlinable public static func * (left: Array, right: Element) -> Array {
+        return vDSP.multiply(right, left)
+    }
+    
+    @inlinable public static func / (left: Array, right: Element) -> Array {
+        return vDSP.divide(left, right)
+    }
+    
+}
+
+extension Array where Element == Float {
+    
+    @inlinable public static func + (left: Element, right: Array) -> Array {
+        return vDSP.add(left, right)
+    }
+    
+    @inlinable public static func - (left: Element, right: Array) -> Array {
+        return -right + left
+    }
+    
+    @inlinable public static func * (left: Element, right: Array) -> Array {
+        return vDSP.multiply(left, right)
+    }
+    
+    @inlinable public static func / (left: Element, right: Array) -> Array {
+        return vDSP.divide(left, right)
+    }
+    
+}
+
+extension Array where Element == Float {
+    
+    @inlinable public static func + (left: Array, right: Array) -> Array {
+        return vDSP.add(left, right)
+    }
+    
+    @inlinable public static func - (left: Array, right: Array) -> Array {
+        return vDSP.subtract(left, right)
+    }
+    
+    @inlinable public static func * (left: Array, right: Array) -> Array {
+        return vDSP.multiply(left, right)
+    }
+    
+    @inlinable public static func / (left: Array, right: Array) -> Array {
+        return vDSP.divide(left, right)
+    }
+    
+}
+
+extension Array where Element == Float {
+    
+    @inlinable public static func += (left: inout Array, right: Element) {
+        left = left + right
+    }
+    
+    @inlinable public static func -= (left: inout Array, right: Element) {
+        left = left - right
+    }
+    
+    @inlinable public static func *= (left: inout Array, right: Element) {
+        left = left * right
+    }
+    
+    @inlinable public static func /= (left: inout Array, right: Element) {
+        left = left / right
+    }
+    
+}
+
+extension Array where Element == Float {
+    
+    @inlinable public static func += (left: inout Array, right: Array) {
+        left = left + right
+    }
+    
+    @inlinable public static func -= (left: inout Array, right: Array) {
+        left = left - right
+    }
+    
+    @inlinable public static func *= (left: inout Array, right: Array) {
+        left = left * right
+    }
+    
+    @inlinable public static func /= (left: inout Array, right: Array) {
+        left = left / right
+    }
+    
+}
 
 extension Matrix where Scalar == Float {
     
@@ -118,6 +230,118 @@ extension Matrix where Scalar == Float {
         left = left / right
     }
 
+}
+
+extension Array where Element == Double {
+    
+    @inlinable public static prefix func + (operand: Array) -> Array {
+        return operand
+    }
+    
+    @inlinable public static prefix func - (operand: Array) -> Array {
+        return vDSP.negative(operand)
+    }
+    
+}
+
+extension Array where Element == Double {
+    
+    @inlinable public static func + (left: Array, right: Element) -> Array {
+        return vDSP.add(right, left)
+    }
+    
+    @inlinable public static func - (left: Array, right: Element) -> Array {
+        return -right + left
+    }
+    
+    @inlinable public static func * (left: Array, right: Element) -> Array {
+        return vDSP.multiply(right, left)
+    }
+    
+    @inlinable public static func / (left: Array, right: Element) -> Array {
+        return vDSP.divide(left, right)
+    }
+    
+}
+
+extension Array where Element == Double {
+    
+    @inlinable public static func + (left: Element, right: Array) -> Array {
+        return vDSP.add(left, right)
+    }
+    
+    @inlinable public static func - (left: Element, right: Array) -> Array {
+        return -right + left
+    }
+    
+    @inlinable public static func * (left: Element, right: Array) -> Array {
+        return vDSP.multiply(left, right)
+    }
+    
+    @inlinable public static func / (left: Element, right: Array) -> Array {
+        return vDSP.divide(left, right)
+    }
+    
+}
+
+extension Array where Element == Double {
+    
+    @inlinable public static func + (left: Array, right: Array) -> Array {
+        return vDSP.add(left, right)
+    }
+    
+    @inlinable public static func - (left: Array, right: Array) -> Array {
+        return vDSP.subtract(left, right)
+    }
+    
+    @inlinable public static func * (left: Array, right: Array) -> Array {
+        return vDSP.multiply(left, right)
+    }
+    
+    @inlinable public static func / (left: Array, right: Array) -> Array {
+        return vDSP.divide(left, right)
+    }
+    
+}
+
+extension Array where Element == Double {
+    
+    @inlinable public static func += (left: inout Array, right: Element) {
+        left = left + right
+    }
+    
+    @inlinable public static func -= (left: inout Array, right: Element) {
+        left = left - right
+    }
+    
+    @inlinable public static func *= (left: inout Array, right: Element) {
+        left = left * right
+    }
+    
+    @inlinable public static func /= (left: inout Array, right: Element) {
+        left = left / right
+    }
+    
+}
+
+extension Array where Element == Double {
+    
+    @inlinable public static func += (left: inout Array, right: Array) {
+        left = left + right
+    }
+    
+    @inlinable public static func -= (left: inout Array, right: Array) {
+        left = left - right
+    }
+    
+    @inlinable public static func *= (left: inout Array, right: Array) {
+        left = left * right
+    }
+    
+    @inlinable public static func /= (left: inout Array, right: Array) {
+        left = left / right
+    }
+    
 }
 
 extension Matrix where Scalar == Double {
