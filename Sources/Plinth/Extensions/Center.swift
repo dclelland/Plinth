b1dfad443a37119e4a99f3f04458f581e112919d
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-26 18:36:55 +1200

    Added Center module

---
+++ Sources/Plinth/Extensions/Center.swift
@@ -0,0 +1,54 @@
+//
+//  Center.swift
+//  Plinth
+//
+//  Created by Daniel Clelland on 26/04/22.
+//
+
+import Foundation
+
+public struct CenterRoundingRule {
+    
+    internal var row: FloatingPointRoundingRule
+    internal var column: FloatingPointRoundingRule
+    
+    internal init(row: FloatingPointRoundingRule, column: FloatingPointRoundingRule) {
+        self.row = row
+        self.column = column
+    }
+    
+    public static let towardsTopLeft = CenterRoundingRule(row: .down, column: .down)
+    public static let towardsTopRight = CenterRoundingRule(row: .down, column: .up)
+    public static let towardsBottomLeft = CenterRoundingRule(row: .up, column: .down)
+    public static let towardsBottomRight = CenterRoundingRule(row: .up, column: .up)
+    
+}
+
+extension Shape {
+    
+    public func center(_ rule: CenterRoundingRule = .towardsTopLeft) -> (row: Int, column: Int) {
+        return (
+            row: Int((Double(rows - 1) / 2.0).rounded(rule.row)),
+            column: Int((Double(columns - 1) / 2.0).rounded(rule.column))
+        )
+    }
+    
+}
+
+extension Matrix {
+    
+    public func center(_ rule: CenterRoundingRule = .towardsTopLeft) -> Scalar {
+        let center = shape.center(rule)
+        return self[center.row, center.column]
+    }
+    
+}
+
+extension ComplexMatrix {
+    
+    public func center(_ rule: CenterRoundingRule = .towardsTopLeft) -> Complex {
+        let center = shape.center(rule)
+        return self[center.row, center.column]
+    }
+    
+}
