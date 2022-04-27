b0cdb110c3095f87992d7d085ae0aae8efb0ab0e
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-27 20:08:21 +1200

    Accept an fft setup argument to autocorrelation methods

--- Sources/Plinth/Extensions/Autocorrelation.swift
+++ Sources/Plinth/Extensions/Autocorrelation.swift
@@ -10,32 +10,32 @@ import Accelerate
 
 extension Matrix where Scalar == Float {
     
-    public func autocorrelated() -> ComplexMatrix<Scalar> {
-        return fft().fmap(vDSP.squareMagnitudes).ifft() / Scalar(shape.count)
+    public func autocorrelated(setup: FFTSetup? = nil) -> ComplexMatrix<Scalar> {
+        return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
 }
 
 extension ComplexMatrix where Scalar == Float {
     
-    public func autocorrelated() -> ComplexMatrix {
-        return fft().fmap(vDSP.squareMagnitudes).ifft() / Scalar(shape.count)
+    public func autocorrelated(setup: FFTSetup? = nil) -> ComplexMatrix {
+        return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
 }
 
 extension Matrix where Scalar == Double {
     
-    public func autocorrelated() -> ComplexMatrix<Scalar> {
-        return fft().fmap(vDSP.squareMagnitudes).ifft() / Scalar(shape.count)
+    public func autocorrelated(setup: FFTSetupD? = nil) -> ComplexMatrix<Scalar> {
+        return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
 }
 
 extension ComplexMatrix where Scalar == Double {
     
-    public func autocorrelated() -> ComplexMatrix {
-        return fft().fmap(vDSP.squareMagnitudes).ifft() / Scalar(shape.count)
+    public func autocorrelated(setup: FFTSetupD? = nil) -> ComplexMatrix {
+        return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
 }
