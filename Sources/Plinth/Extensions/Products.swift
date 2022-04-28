d9ba98df1b575d23c9cee26974dabef82505758b
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-28 23:38:30 +1200

    Add products module

---
+++ Sources/Plinth/Extensions/Products.swift
@@ -0,0 +1,32 @@
+//
+//  Products.swift
+//  Products
+//
+//  Created by Daniel Clelland on 28/04/22.
+//
+
+import Foundation
+
+extension Matrix where Scalar == Float {
+    
+    public func innerProduct() -> Matrix {
+        return transposed() ** self
+    }
+    
+    public func outerProduct() -> Matrix {
+        return self ** transposed()
+    }
+    
+}
+
+extension Matrix where Scalar == Double {
+    
+    public func innerProduct() -> Matrix {
+        return transposed() ** self
+    }
+    
+    public func outerProduct() -> Matrix {
+        return self ** transposed()
+    }
+    
+}
