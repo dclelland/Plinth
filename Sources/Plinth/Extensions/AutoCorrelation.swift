e185c579f473f256cd2c794fb5e26943f7646d17
Daniel Clelland <daniel.clelland@gmail.com>
2022-04-29 14:39:50 +1200

    Rename correlation method

--- Sources/Plinth/Extensions/AutoCorrelation.swift
+++ Sources/Plinth/Extensions/AutoCorrelation.swift
@@ -1,5 +1,5 @@
 //
-//  Autocorrelation.swift
+//  AutoCorrelation.swift
 //  Plinth
 //
 //  Created by Daniel Clelland on 26/04/22.
@@ -10,7 +10,7 @@ import Accelerate
 
 extension Matrix where Scalar == Float {
     
-    public func autocorrelated(setup: FFTSetup? = nil) -> ComplexMatrix<Scalar> {
+    public func autoCorrelated(setup: FFTSetup? = nil) -> ComplexMatrix<Scalar> {
         return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
@@ -18,7 +18,7 @@ extension Matrix where Scalar == Float {
 
 extension ComplexMatrix where Scalar == Float {
     
-    public func autocorrelated(setup: FFTSetup? = nil) -> ComplexMatrix {
+    public func autoCorrelated(setup: FFTSetup? = nil) -> ComplexMatrix {
         return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
@@ -26,7 +26,7 @@ extension ComplexMatrix where Scalar == Float {
 
 extension Matrix where Scalar == Double {
     
-    public func autocorrelated(setup: FFTSetupD? = nil) -> ComplexMatrix<Scalar> {
+    public func autoCorrelated(setup: FFTSetupD? = nil) -> ComplexMatrix<Scalar> {
         return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
@@ -34,7 +34,7 @@ extension Matrix where Scalar == Double {
 
 extension ComplexMatrix where Scalar == Double {
     
-    public func autocorrelated(setup: FFTSetupD? = nil) -> ComplexMatrix {
+    public func autoCorrelated(setup: FFTSetupD? = nil) -> ComplexMatrix {
         return fft(setup: setup).fmap(vDSP.squareMagnitudes).ifft(setup: setup) / Scalar(shape.count)
     }
     
