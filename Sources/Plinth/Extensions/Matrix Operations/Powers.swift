f9ad7a41d9a452a478a036c137b7ee66c9aa1df7
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-01 17:09:47 +1200

    Make pow() implementation more readable

--- Sources/Plinth/Extensions/Matrix Operations/Powers.swift
+++ Sources/Plinth/Extensions/Matrix Operations/Powers.swift
@@ -12,15 +12,11 @@ extension Matrix where Scalar == Float {
     public func pow(_ exponent: Int) -> Matrix {
         switch exponent.signum() {
         case -1:
-            return (1..<(-exponent)).reduce(self) { result, _ in
-                return result * self
-            }.reciprocal()
+            return [Matrix](repeating: self, count: -exponent - 1).reduce(self, *).reciprocal()
         case 0:
             return .init(shape: shape, repeating: 1.0)
         case 1:
-            return (1..<exponent).reduce(self) { result, _ in
-                return result * self
-            }
+            return [Matrix](repeating: self, count: exponent - 1).reduce(self, *)
         default:
             fatalError()
         }
@@ -33,15 +29,11 @@ extension Matrix where Scalar == Double {
     public func pow(_ exponent: Int) -> Matrix {
         switch exponent.signum() {
         case -1:
-            return (1..<(-exponent)).reduce(self) { result, _ in
-                return result * self
-            }.reciprocal()
+            return [Matrix](repeating: self, count: -exponent - 1).reduce(self, *).reciprocal()
         case 0:
             return .init(shape: shape, repeating: 1.0)
         case 1:
-            return (1..<exponent).reduce(self) { result, _ in
-                return result * self
-            }
+            return [Matrix](repeating: self, count: exponent - 1).reduce(self, *)
         default:
             fatalError()
         }
