4add68da730a0c006b87fe553ddf33721d598e5b
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-03 16:18:32 +1200

    Added Random extension

---
+++ Sources/Plinth/Extensions/Statistics/Random.swift
@@ -0,0 +1,64 @@
+//
+//  Random.swift
+//  Plinth
+//
+//  Created by Daniel Clelland on 3/05/22.
+//
+
+import Foundation
+
+extension Matrix where Scalar == Float {
+    
+    public static func random(shape: Shape, in range: Range<Scalar>) -> Matrix {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range)
+        }
+    }
+    
+    public static func random<T>(shape: Shape, in range: Range<Scalar>, using generator: inout T) -> Matrix where T : RandomNumberGenerator {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range, using: &generator)
+        }
+    }
+    
+    public static func random(shape: Shape, in range: ClosedRange<Scalar>) -> Matrix {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range)
+        }
+    }
+    
+    public static func random<T>(shape: Shape, in range: ClosedRange<Scalar>, using generator: inout T) -> Matrix where T : RandomNumberGenerator {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range, using: &generator)
+        }
+    }
+    
+}
+
+extension Matrix where Scalar == Double {
+    
+    public static func random(shape: Shape, in range: Range<Scalar>) -> Matrix {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range)
+        }
+    }
+    
+    public static func random<T>(shape: Shape, in range: Range<Scalar>, using generator: inout T) -> Matrix where T : RandomNumberGenerator {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range, using: &generator)
+        }
+    }
+    
+    public static func random(shape: Shape, in range: ClosedRange<Scalar>) -> Matrix {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range)
+        }
+    }
+    
+    public static func random<T>(shape: Shape, in range: ClosedRange<Scalar>, using generator: inout T) -> Matrix where T : RandomNumberGenerator {
+        return Matrix(shape: shape) { row, column in
+            return Scalar.random(in: range, using: &generator)
+        }
+    }
+    
+}
