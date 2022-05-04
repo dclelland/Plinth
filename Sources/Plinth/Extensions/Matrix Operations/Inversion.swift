d80688d0f1c11ee037c0de238913f69a913c7b34
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-04 18:58:32 +1200

    Implemented matrix inversion

---
+++ Sources/Plinth/Extensions/Matrix Operations/Inversion.swift
@@ -0,0 +1,51 @@
+//
+//  Inversion.swift
+//  Plinth
+//
+//  Created by Daniel Clelland on 4/05/22.
+//
+
+import Foundation
+import Accelerate
+
+extension Matrix where Scalar == Float {
+    
+    public func inverted() -> Matrix {
+        precondition(shape.isSquare)
+        
+        var input = self
+        var length = __CLPK_integer(shape.length)
+        var pivots = [__CLPK_integer](repeating: .zero, count: shape.length)
+        var workspace = [Scalar](repeating: .zero, count: shape.length)
+        var error: __CLPK_integer = 0
+        
+        withUnsafeMutablePointer(to: &length) { length in
+            sgetrf_(length, length, &input.elements, length, &pivots, &error)
+            sgetri_(length, &input.elements, length, &pivots, &workspace, length, &error)
+        }
+        
+        return input
+    }
+    
+}
+
+extension Matrix where Scalar == Double {
+    
+    public func inverted() -> Matrix {
+        precondition(shape.isSquare)
+        
+        var input = self
+        var length = __CLPK_integer(shape.length)
+        var pivots = [__CLPK_integer](repeating: .zero, count: shape.length)
+        var workspace = [Scalar](repeating: .zero, count: shape.length)
+        var error: __CLPK_integer = 0
+        
+        withUnsafeMutablePointer(to: &length) { length in
+            dgetrf_(length, length, &input.elements, length, &pivots, &error)
+            dgetri_(length, &input.elements, length, &pivots, &workspace, length, &error)
+        }
+        
+        return input
+    }
+    
+}
