aaed13d03b864cc31a08ec477ed110fcce780bfc
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-11 11:58:04 +1200

    Write the identity matrix in a better way

--- Sources/Plinth/Extensions/Linear Algebra/Identity.swift
+++ Sources/Plinth/Extensions/Linear Algebra/Identity.swift
@@ -11,7 +11,7 @@ extension Matrix where Scalar: Numeric {
     
     public static func identity(shape: Shape) -> Matrix {
         return .init(shape: shape) { row, column in
-            return column - row == 0 ? 1 : 0
+            return row == column ? 1 : 0
         }
     }
     
