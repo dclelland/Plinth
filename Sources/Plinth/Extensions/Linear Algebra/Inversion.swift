a75fb56618078a4ea8753b77cabb910cf8485bea
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-31 00:09:22 -0700

    Set up complex matrix inversion

--- Sources/Plinth/Extensions/Linear Algebra/Inversion.swift
+++ Sources/Plinth/Extensions/Linear Algebra/Inversion.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Accelerate
+import Numerics
 
 public enum InversionError<Scalar>: LocalizedError {
     
@@ -19,6 +20,22 @@ public enum InversionError<Scalar>: LocalizedError {
             return "Matrix inversion error: Illegal value (\(code))"
         case .computationFailed(_, let code):
             return "Matrix inversion error: Computation failed (\(code))"
+        }
+    }
+    
+}
+
+public enum ComplexInversionError<Scalar>: LocalizedError where Scalar: Real {
+    
+    case illegalValue(matrix: ComplexMatrix<Scalar>, code: Int32)
+    case computationFailed(matrix: ComplexMatrix<Scalar>, code: Int32)
+    
+    public var errorDescription: String? {
+        switch self {
+        case .illegalValue(_, let code):
+            return "Complex matrix inversion error: Illegal value (\(code))"
+        case .computationFailed(_, let code):
+            return "Complex matrix inversion error: Computation failed (\(code))"
         }
     }
     
@@ -56,8 +73,28 @@ extension Matrix where Scalar == Float {
 extension ComplexMatrix where Scalar == Float {
     
     public func inverted() throws -> ComplexMatrix {
-        #warning("Implement ComplexMatrix<Float>.inverted()")
-        fatalError()
+        precondition(shape.isSquare)
+        
+        var input = zip(real, imaginary).map { __CLPK_complex(r: $0, i: $1) }
+        var length = __CLPK_integer(shape.length)
+        var pivots = [__CLPK_integer](repeating: .zero, count: shape.length)
+        var workspace = [__CLPK_complex](repeating: __CLPK_complex(), count: shape.length)
+        var error: __CLPK_integer = 0
+        
+        withUnsafeMutablePointer(to: &length) { length in
+            cgetrf_(length, length, &input, length, &pivots, &error)
+            cgetri_(length, &input, length, &pivots, &workspace, length, &error)
+        }
+        
+        if error < 0 {
+            throw ComplexInversionError.illegalValue(matrix: self, code: error)
+        }
+        
+        if error > 0 {
+            throw ComplexInversionError.computationFailed(matrix: self, code: error)
+        }
+        
+        return ComplexMatrix(real: .init(shape: shape, elements: input.map(\.r)), imaginary: .init(shape: shape, elements: input.map(\.i)))
     }
     
 }
@@ -94,8 +131,28 @@ extension Matrix where Scalar == Double {
 extension ComplexMatrix where Scalar == Double {
     
     public func inverted() throws -> ComplexMatrix {
-        #warning("Implement ComplexMatrix<Double>.inverted()")
-        fatalError()
+        precondition(shape.isSquare)
+        
+        var input = zip(real, imaginary).map { __CLPK_doublecomplex(r: $0, i: $1) }
+        var length = __CLPK_integer(shape.length)
+        var pivots = [__CLPK_integer](repeating: .zero, count: shape.length)
+        var workspace = [__CLPK_doublecomplex](repeating: __CLPK_doublecomplex(), count: shape.length)
+        var error: __CLPK_integer = 0
+        
+        withUnsafeMutablePointer(to: &length) { length in
+            zgetrf_(length, length, &input, length, &pivots, &error)
+            zgetri_(length, &input, length, &pivots, &workspace, length, &error)
+        }
+        
+        if error < 0 {
+            throw ComplexInversionError.illegalValue(matrix: self, code: error)
+        }
+        
+        if error > 0 {
+            throw ComplexInversionError.computationFailed(matrix: self, code: error)
+        }
+        
+        return ComplexMatrix(real: .init(shape: shape, elements: input.map(\.r)), imaginary: .init(shape: shape, elements: input.map(\.i)))
     }
     
 }
